3a5f80853649f2a4da8c0e9ae7c3bdeaecee3559
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2024-01-28 11:30:37 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Practice_Constructors_Methods_Overloading_JuanHernandez
+{
+    internal class Program
+    {
+        // Juan Hernandez
+        // Practice: Constructors, Methods, and Overloading
+        // 01/28/2024
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
