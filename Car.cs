fc4ac59464d69d1376e0a5ba9ba0d7190b12a325
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2024-01-28 19:07:08 -0800

    Part1

---
+++ Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Constructors_Methods_Overloading_JuanHernandez
+{
+    public class Car
+    {
+        // Variables
+        string _make;
+        string _model;
+        int _year;
+        int _mileage;
+        
+        // Properties
+        public string Make { get => _make; set => _make = value; }
+        public string Model { get => _model; set => _model = value; }
+        public int Year { get => _year; set => _year = value; }
+        public int Mileage { get => _mileage; set => _mileage = value; }
+
+        public Car() // default constructor
+        {
+            Make = "unassigned";
+            Model = "unassigned";
+            Year = 0;
+            Mileage = 0;
+        }
+        public Car(string make, string model) // constructor: takes 2 variables
+        {
+            Make = make;    
+            Model = model;  
+            Year = 0;
+            Mileage = 0;
+        }
+        public Car(string make, string model, int year, int mileage) // constructor: tales 4 variables
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Mileage = mileage;
+        }
+        public override string ToString()
+        {
+            return $"{Year} {Make} {Model} - {Mileage}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,15 @@
         // 01/28/2024
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Code Test
+            Car defaultCar = new Car();
+            Car makeModelCar = new Car("Toyota", "Rav4");
+            Car fullCar = new Car("Hyundai", "Sonata", 2010, 100000);
+
+            // Display
+            Console.WriteLine(defaultCar);
+            Console.WriteLine(makeModelCar);
+            Console.WriteLine(fullCar);
         }
     }
 }
